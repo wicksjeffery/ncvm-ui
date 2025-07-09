37d984c2e3d8cee1fb95894abb0160c8e8754767
Jeff Wicks <wicksjeffery1@gmail.com>
2025-07-09 01:04:20 -0400

    Clean up. Also, todo - stop writing ncvm.log.

--- logging/manager.hpp
+++ logging/manager.hpp
@@ -13,51 +13,24 @@
 #include <unordered_map>
 namespace Logging
 {
-
     class Manager
     {
-        // Private constructor to prevent direct instantiation
         Manager();
         ~Manager();
-        // Delete copy constructor and assignment operator to prevent copying
         Manager(const Manager&) = delete;
         Manager& operator=(const Manager&) = delete;
 
-
-        void j_open();
-        void j_match();
-        int j_next(unsigned short);
-        int j_wait(unsigned short);
-        void j_get_data(unsigned short);
-
-
-        std::array<sd_journal* , 1> journal_objects;
-
-        // SDJ sdj1;
-
-        void runSDJ1();
-        void runSDJ1OLD2();
-
+        bool running = true;
         unsigned short sdj_throttle = 0;
 
-        // std::unordered_map<SDJ(const char*), unsigned short> m_sdjs;
-        // std::unordered_map<unsigned short, SDJ(const char*)> m_sdjs;
-
     public:
-        // Static method to get the single instance of Manager
         static Manager& getInstance();
 
         void write(std::string);
 
-        int run1(int);
         int run(int);
-        int start();
 
         void quit() { running = false; }
-
-        bool running = true;
-
-
     };
 }
 #endif
